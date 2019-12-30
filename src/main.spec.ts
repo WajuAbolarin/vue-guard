1550aac09b54ba420835d119926c9684d72ba017
Abolarin Olanrewaju Olabode <waju.abolarin@gmail.com>
2019-12-30 09:25:34 +0100

    add first unit test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,4 +1,12 @@
 import { isPermitted } from "./main";
+let Permissions = {
+  CREATE: "create apps",
+  VIEW: "view apps",
+  EDIT: "edit apps",
+  DELETE: "delete apps"
+};
+let usersPermissions = [Permissions.CREATE, Permissions.VIEW];
 test("smoke test", () => {
-  expect(isPermitted(["a"], ["b"], true)).toBeFalsy();
+  expect(isPermitted(usersPermissions, [Permissions.DELETE], true)).toBeFalsy();
+  expect(isPermitted(usersPermissions, [Permissions.VIEW], true)).toBeTruthy();
 });
